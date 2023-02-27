e814607827676bc12517a2cc1d7e19223cb99f2c
Resul Emre AYGAN <remreaygan@gmail.com>
2023-02-27 23:36:18 +0300

    added some exercises about flow control

--- flowcontrol.go
+++ flowcontrol.go
@@ -7,6 +7,8 @@ package main
 import (
 	"fmt"
 	"math"
+	"runtime"
+	"time"
 )
 
 func sqrt(x float64) string {
@@ -77,4 +79,60 @@ func main() {
 		powNew(3, 2, 10),
 		powNew(3, 3, 20),
 	)
+
+	//Switch
+	fmt.Print("Go runs on ")
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("OS X.")
+	case "linux":
+		fmt.Println("Linux.")
+	default:
+		// freebsd, openbsd,
+		// plan9, windows..
+
+		fmt.Printf("%s.\n", os)
+	}
+
+	//Switch evaluation order
+	fmt.Println("When's Saturday?")
+	today := time.Now().Weekday()
+
+	switch time.Saturday {
+	case today + 0:
+		fmt.Println("Today.")
+	case today + 1:
+		fmt.Println("Tomorrow.")
+	case today + 2:
+		fmt.Println("In tow days.")
+	default:
+		fmt.Println("Too far away.")
+	}
+
+	//Switch with no condition
+	t := time.Now()
+
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("Good Morning!")
+	case t.Hour() < 17:
+		fmt.Println("Good afternoon.")
+	default:
+		fmt.Println("Good evening.")
+	}
+
+	//Defer
+	defer fmt.Println("world")
+
+	fmt.Println("Hello")
+
+	//Stacking defers
+	fmt.Println("counting")
+
+	for i := 0; i < 10; i++ {
+		defer fmt.Println(i)
+		//resource leak in loop
+	}
+
+	fmt.Println("done")
 }
