90525986f747699f8feabb5f2de34bb25c3a1f71
Resul Emre AYGAN <remreaygan@gmail.com>
2023-03-02 01:29:06 +0300

    added slices-literals

--- arrays.go
+++ arrays.go
@@ -41,4 +41,25 @@ func main() {
 	fmt.Println(aNew, bNew)
 	fmt.Println(names)
 	// Changing the elements of a slice modifies the corresponding elements of its underlying array.
+
+	// Slice literals
+	q := []int{2, 3, 5, 7, 11, 13}
+	fmt.Println(q)
+
+	r := []bool{true, false, true, true, false, true}
+	fmt.Println(r)
+
+	sNew := []struct {
+		i int
+		b bool
+	}{
+		{2, true},
+		{3, false},
+		{5, true},
+		{7, true},
+		{11, false},
+		{13, true},
+	}
+
+	fmt.Println(sNew)
 }
