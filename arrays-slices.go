c8b097fb1e3beb6ce1a7515574b4d81700248ec7
Resul Emre AYGAN <remreaygan@gmail.com>
2023-03-02 01:46:03 +0300

    slices of slice

--- arrays-slices.go
+++ arrays-slices.go
@@ -4,7 +4,10 @@ Author: Resul Emre AYGAN
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	//Arrays
@@ -114,6 +117,27 @@ func main() {
 
 	j := g[2:5]
 	printSliceNew("j", j)
+
+	//Slices of slices
+
+	// Create a tic-tac-toe board.
+	board := [][]string{
+		[]string{"_", "_", "_"},
+		[]string{"_", "_", "_"},
+		[]string{"_", "_", "_"},
+	}
+
+	// The players take turns.
+	board[0][0] = "X"
+	board[2][2] = "O"
+	board[1][2] = "X"
+	board[1][0] = "O"
+	board[0][2] = "X"
+
+	for i := 0; i < len(board); i++ {
+		fmt.Printf("%s\n", strings.Join(board[i], " "))
+	}
+
 }
 
 func printSlice(l []int) {
