9f4b2f35c48399ec7cc6e1926280d6a497823022
Resul Emre AYGAN <remreaygan@gmail.com>
2023-03-03 00:57:05 +0300

    added maps literals

--- maps.go
+++ maps.go
@@ -12,9 +12,17 @@ type VertexNew struct {
 
 var m map[string]VertexNew
 
+var n = map[string]VertexNew{
+	"Bell Labs": VertexNew{40.68433, -74.39967},
+	"Google":    VertexNew{37.42202, -122.08408},
+}
+
 func main() {
 	//Maps
 	m = make(map[string]VertexNew)
 	m["Bell Labs"] = VertexNew{40.68433, -74.39967}
 	fmt.Println(m["Bell Labs"])
+
+	//Map literals
+	fmt.Println(n)
 }
