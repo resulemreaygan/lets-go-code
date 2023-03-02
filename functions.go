23d9b130eb9cf02aee3ca54dfc5a546eca7ab592
Resul Emre AYGAN <remreaygan@gmail.com>
2023-03-03 01:07:48 +0300

    added functions

---
+++ functions.go
@@ -0,0 +1,27 @@
+/*
+Author: Resul Emre AYGAN
+*/
+
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func compute(fn func(float64, float64) float64) float64 {
+	return fn(3, 4)
+}
+
+func main() {
+	//Function values
+	hypot := func(x, y float64) float64 {
+		return math.Sqrt(x*x + y*y)
+	}
+
+	fmt.Println(hypot(5, 12))
+
+	fmt.Println(compute(hypot))
+	fmt.Println(compute(math.Pow))
+
+}
