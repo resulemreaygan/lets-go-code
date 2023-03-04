b1d50edc9a50b9f7306de26b4fc1a7de01f9360b
Resul Emre AYGAN <remreaygan@gmail.com>
2023-03-04 16:44:27 +0300

    methods continued

--- methods.go
+++ methods.go
@@ -13,12 +13,21 @@ type Vertex2 struct {
 	X, Y float64
 }
 
+type MyFloat float64
+
 func (v Vertex2) Abs() float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
 func AbsNew(v Vertex2) float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func (f MyFloat) AbsNew2() float64 {
+	if f < 0 {
+		return float64(-f)
+	}
+	return float64(f)
 }
 
 func main() {
@@ -28,4 +37,8 @@ func main() {
 
 	//Methods are functions
 	fmt.Println(AbsNew(v))
+
+	//Methods continued
+	f := MyFloat(-math.Sqrt2)
+	fmt.Println(f.AbsNew2())
 }
