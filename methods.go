2b9547670c86fbb7ca4016f23cdc125741296714
Resul Emre AYGAN <remreaygan@gmail.com>
2023-03-04 16:56:24 +0300

    added pointers and funcs

--- methods.go
+++ methods.go
@@ -35,6 +35,11 @@ func (v *Vertex2) Scale(f float64) {
 	v.Y = v.Y * f
 }
 
+func ScaleNew(v *Vertex2, f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
+
 func main() {
 	//Methods
 	v := Vertex2{3, 4}
@@ -50,4 +55,9 @@ func main() {
 	//Pointer receivers
 	v.Scale(10)
 	fmt.Println(v.Abs())
+
+	//Pointers and functions
+	vNew := Vertex2{3, 4}
+	ScaleNew(&vNew, 10)
+	fmt.Println(AbsNew(vNew))
 }
