7fdb6c4e4ce5e5ca80de3df11ca86d5a821b35c6
Resul Emre AYGAN <remreaygan@gmail.com>
2023-03-27 00:56:59 +0300

    added type switches

---
+++ type-switches.go
@@ -0,0 +1,27 @@
+/*
+Author: Resul Emre AYGAN
+*/
+
+package main
+
+import "fmt"
+
+func do(i interface{}) {
+	switch v := i.(type) {
+	case int:
+		fmt.Printf("Twice %v is %v\n", v, v*2)
+	case string:
+		fmt.Printf("%q is %v bytes long\n", v, len(v))
+	default:
+		fmt.Printf("I don't know about type %T!\n", v)
+	}
+}
+
+func main() {
+	//Type switches
+
+	do(58)
+	do("REA")
+	do(true)
+	do(34.0)
+}
